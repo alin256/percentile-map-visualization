4ee2ea66a4feb5d310d8687613ec94efef2825fa
Sergey Alyaev <cobxo3bot@gmail.com>
2026-07-01 13:10:28 +0200

    Add better color representation

--- visualize_ps.py
+++ visualize_ps.py
@@ -51,20 +51,30 @@ sorted_percentiles = np.sort(percentiles.ravel())
 n = len(sorted_percentiles)
 theoretical = np.linspace(0, 100, n)
 
+COLORMAP = "cividis"
+
 plt.figure("Sorted Percentiles", figsize=(10, 6))
 
 n_cols = percentiles.shape[1]
-colors = plt.cm.tab20(np.linspace(0, 1, n_cols))
 
-highlighted = list(range(6)) + list(range(10, n_cols, 10))
+highlighted = list(range(6)) + [10] + list(range(20, n_cols, 20))
+
+n_colors = len(highlighted)
+
+colors = matplotlib.colormaps[COLORMAP](np.linspace(0, 1, n_cols))
+
 
 plt.figure("Sorted Percentiles", figsize=(10, 6))
 
-for col_idx in range(n_cols):
+for col_idx_hl in highlighted:
+    col_idx = highlighted[col_idx_hl]
     col_sorted = np.sort(percentiles[:, col_idx])
     x = np.linspace(0, 1, len(col_sorted))
     if col_idx in highlighted:
-        plt.plot(x, col_sorted, color=colors[col_idx], alpha=0.9, linewidth=0.5, label=f"Column {col_idx}")
+        plt.plot(x, col_sorted, 
+                 color=colors[col_idx_hl], 
+                 #  alpha=0.9, 
+                 linewidth=0.5, label=f"Column {col_idx}")
     else:
         pass
         # plt.plot(x, col_sorted, color=colors[col_idx], alpha=0.2, linewidth=0.5)
@@ -78,7 +88,7 @@ plt.title("Sorted percentiles — one curve per column")
 plt.legend(fontsize=7, ncol=2)
 plt.grid(True)
 
-sm = plt.cm.ScalarMappable(cmap="tab20", norm=matplotlib.colors.Normalize(vmin=0, vmax=n_cols - 1))
+sm = plt.cm.ScalarMappable(cmap=COLORMAP, norm=matplotlib.colors.Normalize(vmin=0, vmax=n_cols - 1))
 plt.colorbar(sm, ax=plt.gca(), label="Column index (left → right)")
 
 plt.show()
